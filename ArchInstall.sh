8d2ab7b4fc49744b33fcc191c8fc11d2abf14afa
Edmilson-Hao <edmilsonwp@gmail.com>
2018-12-10 01:37:35 +0000

    First script.

---
+++ ArchInstall.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#################################Variables###########################################
+
+set partition
+
+#####################################################################################
+
+loadkeys br-abnt2
+
+clear
+function setPartition() {
+	echo "This script assumes that you have already formated the driver to a layout that"
+	echo "suits your need also if you want to create a swap driver make it yourself by"
+	echo "using mkswap and swapon. I think using a swapfile is a better idea."
+
+	echo ; echo
+	sleep 1
+
+	echo "Please enter the partition you want to install arch linux (eg.: sda1 or sd2...)."
+	echo -e "\a"
+
+	read partition
+	clear
+
+	mount /dev/$partition /mnt && echo "/dev/$partition mounted in /mnt" ||  { echo "Error: Please check if your typed the right partition and rettry." ; exit ; }
+	sleep 1
+}
+
+function selectMirror() {
+	cat /etc/pacman.d/mirrorlist > /etc/pacman.d/mirrorlist.backup
+	cat /etc/pacman.d/mirrorlist | grep "\.br" > mirror
+	cat mirror > /etc/pacman.d/mirrorlist
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ArchLinuxInstall
+Welcome to the Arch Linux Script Installer.
+This script was created to automate the process of the Arch linux system installation proccess.
+At first its just a small thing and I am no professional at bash script but it will soon be a
+great tool to automate the proccess of having a very minimal installation.
