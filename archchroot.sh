c1e843805e54e0ad1b6bc76ccd8655dc4c62a3bf
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-10 14:19:58 -0200

    Update archchroot.sh

--- archchroot.sh
+++ archchroot.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+exitMessage="The installation is complete, type exit to leave chroot, remove the installation media and reboot the system."
+
 echo "setting pt_BR to locale.gen"
 echo "pt_BR.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
@@ -9,7 +11,15 @@ echo "If your not sure run select another tty and list all available options -> 
 read myzoneinfo
 ln -sf /usr/share/zoneinfo/$myzoneinfo /etc/localtime
 sleep 1
+clear
 
+echo "Do you want to install GRUB in the MBR? (Y/n)"
+read -e option
+#Ask if user wants to install grub
+[[ ! $option == "y" ]] && { echo $exitMessage ; exit ; }
+[[ ! $option == "Y" ]] && { echo $exitMessage ; exit ; }
+
+clear
 echo "Enter the disk to install GRUB (not the partition but the disk itself eg.: sda, sdb)"
 read disk
 $(grub-install /dev/$disk)
