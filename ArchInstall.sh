8b3545dc6359412783b536a103cd9ba11c6d5609
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-10 14:20:34 -0200

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -1,29 +1,13 @@
 #!/usr/bin/env bash
 
-#################################Variables###########################################
+##############################################Functions##############################################
 
-set partition
-
-#####################################################################################
-
-[[ $(ping -c2 archlinux.org) ]] || echo "You don't have internet connection. Please connect a ethernet connection or use wifi-menu to set up a wifi networkin."
-
-clear
 function setPartition() {
-	echo "This script assumes that you have already formated the driver to a layout that"
-	echo "suits your need also if you want to create a swap driver make it yourself by"
-	echo "using mkswap and swapon. I think using a swapfile is a better idea."
-	echo ; echo
-	sleep 1
-
 	echo "Please enter the partition you want to install arch linux (eg.: sda1 or sd2...)."
-	echo -e "\a"
-
 	read partition
 	clear
 
-	mount /dev/$partition /mnt && echo "/dev/$partition mounted in /mnt" ||  { echo "Error: Please check if your typed the right partition and rettry." ; exit ; }
-	sleep 1
+	mount /dev/$partition /mnt && echo "/dev/$partition mounted in /mnt" || { echo "Error: Please check if your typed the right partition and rettry." ; echo " Press any key to continue." ; read ; exit ; }	
 }
 
 function selectMirror() {
@@ -51,5 +35,42 @@ function grubChroot() {
 	echo "/tmp/archchroot.sh" > /mnt/root/.bashrc
 	for i in /sys /proc /run /dev ; do mount --bind "$i" "/mnt$i"; done
 	chroot /mnt /bin/bash
-	echo "" > /mnt/root/.bashrc
 }
+
+####################################################################################################
+
+#Connection test
+[[ $(ping -c2 archlinux.org) ]] || { echo "You don't have internet connection. Please connect a ethernet connection or use wifi-menu to set up a wifi networkin." ; exit ; }
+
+#Information about how partitioning (Does not) work on this script.
+echo "This script assumes that you have already formated the driver to a layout that"
+echo "suits your need also if you want to create a swap driver make it yourself by"
+echo "using mkswap and swapon. I think using a swapfile is a better idea."
+echo
+echo "Press enter to continue."
+read
+clear
+
+option=1
+while [ $option -ne 5 ] ; do
+  echo "******************************************************************************"
+  echo "*     1 - Select Disk Partition.                                             *"
+  echo "*     2 - Select Mirrors.                                                    *"
+  echo "*     3 - Install Base System.                                               *"
+  echo "*     4 - Configure The Installation And Install GRUB to MBR (Optional).     *"
+  echo "*     5 - Exit.                                                              *"
+  echo "******************************************************************************"
+  
+  local PS1="Select your choise: "
+  read -e option
+
+  clear
+
+  case $option in
+  	1) setPartition ;;
+  	2) selectMirror ;;
+  	3) baseInstall ;;
+  	4) grubChroot ; break ;;
+  	5) break ;;
+  esac
+done
