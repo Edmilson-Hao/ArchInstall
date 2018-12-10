2cd4040eee6aaeffb8a4498656a419cfde3ae0de
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-10 09:21:06 -0200

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -36,9 +36,19 @@ function selectMirror() {
 }
 
 function baseInstall() {
-	pacstrap /mnt base base-devel
+	pacstrap /mnt base base-devel grub
 	echo -e "\a"
 }
 
-baseInstall
+function grubChroot() {
+	wget https://raw.githubusercontent.com/Edmilson-Hao/ArchLinuxInstall/master/archchroot.sh
+	cp archchroot.sh /mnt/tmp
+	chmod +x /mnt/tmp/archchroot.sh
 
+	echo "/tmp/archchroot.sh" > /mnt/root/.bashrc
+	for i in /sys /proc /run /dev ; do mount --bind "$i" "/mnt$i"; done
+	chroot /mnt /bin/bash
+	echo "" > /mnt/root/.bashrc
+
+	genfstab /mnt >> /mnt/etc/fstab
+}
