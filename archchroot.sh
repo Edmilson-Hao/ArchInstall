0d65d049613e6aecf429b6f3a56084667191ec5c
Edmilson-Hao <edmilsonwp@gmail.com>
2018-12-11 01:00:12 +0000

    Added grub configuration option.

--- archchroot.sh
+++ archchroot.sh
@@ -2,25 +2,31 @@
 
 exitMessage="The installation is complete, type exit to leave chroot, remove the installation media and reboot the system."
 
-echo "Pleaze enter your current timezone (eg.: America/Recife, America/Argentina/Buenos_aires etc.)."
-echo "If your not sure run select another tty and list all available options -> ls /usr/share/zoneinfo"
-read myzoneinfo
-ln -sf /usr/share/zoneinfo/$myzoneinfo /etc/localtime
-sleep 1
-clear
+function installGrub() {
+	echo "Enter the disk to install GRUB (not the partition but the disk itself eg.: sda, sdb)"
+	read disk
+	clear
+	grub-install /dev/$disk
+	grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+##echo "Pleaze enter your current timezone (eg.: America/Recife, America/Argentina/Buenos_aires etc.)."
+##echo "If your not sure run select another tty and list all available options -> ls /usr/share/zoneinfo"
+##read myzoneinfo
+##ln -sf /usr/share/zoneinfo/$myzoneinfo /etc/localtime
+##sleep 1
+##clear
 #Making initialRam disk file
-mkinitcpio -p linux
+##mkinitcpio -p linux
 
-echo "Do you want to install GRUB in the MBR? (Y/n)"
-read -e option
+##echo "Do you want to install GRUB in the MBR? (Y/n)"
+##read -e option
 #Ask if user wants to install grub
-[[ ! $option == "y" ]] && { echo $exitMessage ; exit ; }
-[[ ! $option == "Y" ]] && { echo $exitMessage ; exit ; }
+echo "Do you want to install grub to MBR?"
+echo "1 - Yes"
+echo "2 - No"
+read grubOption
 
-clear
-echo "Enter the disk to install GRUB (not the partition but the disk itself eg.: sda, sdb)"
-read disk
-$(grub-install /dev/$disk)
-grub-mkconfig -o /boot/grub/grub.cfg
+[[ $grubOption -eq 1 ]] && installGrub
 
 exit
