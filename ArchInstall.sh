a9980360687cfa2c1c1efff8b9dcc7ed552d5f43
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-12 18:54:04 -0200

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -31,77 +31,118 @@
 #SOFTWARE.
 #
 
+##############################################Variables##############################################
+set partition=0
+#####################################################################################################
+
 ##############################################Functions##############################################
 
+#[[ $(id -u) = 0 ]] || { echo "You must be root to execute script as root" ; exit ; }
+
+#Enter partition to install the system
 function setPartition() {
-	echo "Please enter the partition you want to install arch linux (e.g.: sda1 or sd2...)."
+	if [[ $partition -eq 0 ]] ; then
+		echo "Please enter the partition you want to install arch linux (e.g.: sda1 or sd2...): "
+		read partition
+	fi
+
 	clear
-	read partition
 	mkfs.xfs -f /dev/$partition
 	mount /dev/$partition /mnt && echo "/dev/$partition mounted in /mnt" || { echo "Error: Please check if you have typed the right partition and try again." ; echo -e "\nPress any key to continue." ; read ; exit ; }
 }
 
+#select the mirrors
 function selectMirror() {
 	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 	cat /etc/pacman.d/mirrorlist.backup | grep "\.br" > /etc/pacman.d/mirrorlist
 }
 
+#install the base of the system
 function baseInstall() {
 	pacstrap /mnt base base-devel grub
 	echo -e "\a"
 }
 
+#chroot into new installation, configure it and asks if grub installation is needed
 function grubChroot() {
+	#Generating fstab file
 	echo "Generating fstab file."
 	sleep 1
 	genfstab /mnt >> /mnt/etc/fstab
 	
+	#Downloading chroot script
 	echo "Downloading chroot script."
 	sleep 1
 	wget https://raw.githubusercontent.com/Edmilson-Hao/ArchLinuxInstall/master/archchroot.sh
 	cp archchroot.sh /mnt/tmp
 	chmod +x /mnt/tmp/archchroot.sh
 
+	#Configuring new system to start archroot.sh on chroot
 	echo "/tmp/archchroot.sh" > /mnt/root/.bashrc
 	for i in /sys /proc /run /dev ; do mount --bind "$i" "/mnt$i"; done
 	chroot /mnt /bin/bash
 }
 
+#Information about how partitioning (Does not) work on this script.
+function installerInfo(){
+	clear
+	echo
+	echo
+	echo "Before proceeding, make sure the disk has a partition available for installation..."
+	echo
+	echo
+	echo "Press enter to continue (ctrl+c to quit)."
+	read
+}
+
+#The function that show the menu
+function installerMenu(){
+	echo "********************************************************************************"
+	echo "*     (1) - Select Disk Partition.                                             *"
+	echo "*     (2) - Select Mirrors (Brazil only).                                      *"
+	echo "*     (3) - Install Base System.                                               *"
+	echo "*     (4) - Configure The Installation And Install GRUB to MBR (Optional).     *"
+	echo "*     (5) - Lazy mode (Brazil only).										     *"
+	echo "*     (6) - Exit.                                                              *"
+	echo "********************************************************************************"
+  
+	echo "Select your choise: "
+}
+
+function lazyMode() {
+	echo "Root partition (sda1, sda2 etc: "
+	read partition
+	setPartition
+	selectMirror
+	baseInstall
+	grubChroot
+}
 ####################################################################################################
 
 #Connection test
 echo "Testing your connection."
 [[ $(ping -c2 archlinux.org) ]] || { echo "You don't have internet connection. Please connect a ethernet connection or use wifi-menu to set up a wifi networkin." ; exit ; }
 
-#Information about how partitioning (Does not) work on this script.
-echo "This script assumes that you have already defined and formated the disk partitioning that"
-echo "suits your need also it wont create a swap partition, if you need a swap space"
-echo "make a swapfile later."
-echo
-echo "Press enter to continue."
-read
-clear
 
+
+installerInfo
+
+clear
 option=1
 while [ $option -ne 5 ] ; do
-  echo "******************************************************************************"
-  echo "*     1 - Select Disk Partition.                                             *"
-  echo "*     2 - Select Mirrors (Brazil only).                                                    *"
-  echo "*     3 - Install Base System.                                               *"
-  echo "*     4 - Configure The Installation And Install GRUB to MBR (Optional).     *"
-  echo "*     6 - Exit.                                                              *"
-  echo "******************************************************************************"
+
+	installerMenu
+	
+	read option
   
-  echo "Select your choise: "
-  read -p option
+	clear
 
-  clear
-
-  case $option in
-  	1) setPartition ;;
-  	2) selectMirror ;;
-  	3) baseInstall ;;
-  	4) grubChroot ; break ;;
-  	5) break ;;
-  esac
+	case $option in
+		1) setPartition ;;
+		2) selectMirror ;;
+		3) baseInstall ;;
+		4) grubChroot ; break ;;
+		5) lazyMode ; break ;;
+		6) break ;;
+	esac
 done
